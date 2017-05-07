30513bb4534a45652b0456cc45cf27c606941132
Jay Lawrence <jay@patientway.com>
2017-05-07 11:13:11 -0400

    renamed bxslider.css.scss to bxslider.css per Pull request #13

--- update.sh
+++ update.sh
@@ -19,8 +19,8 @@ cp $SRC/vendor/jquery.easing.1.3.js vendor/assets/javascripts
 cp $SRC/vendor/jquery.fitvids.js vendor/assets/javascripts
 
 echo "Copying stylesheets"
-cp $SRC/jquery.bxslider.css vendor/assets/stylesheets/bxslider.css.scss
+cp $SRC/jquery.bxslider.css vendor/assets/stylesheets/bxslider.scss
 echo "Updating stylesheets"
-sed -i '' -e "s/url('images\//url('/g" vendor/assets/stylesheets/bxslider.css.scss
+sed -i '' -e "s/url('images\//url('/g" vendor/assets/stylesheets/bxslider.scss
 
 echo "Done."
